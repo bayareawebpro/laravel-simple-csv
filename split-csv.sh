4556b67ee6a23d13c3dc7586c802f80ae5e2969f
Dan Alvidrez <dan@bayareawebpro.com>
2018-07-30 23:42:20 -0700

    Updated FileSplitter Utility

--- split-csv.sh
+++ split-csv.sh
@@ -5,15 +5,15 @@ _INDEX=1
 _HEADER=$(head -1 "${_PATH}")
 _NAME=$(basename "${_PATH}" .csv)
 _DIR=$(dirname -- "${_PATH}")
-split -l "${_CHUNKS}" "${_PATH}" _LINES
 
-for _LINE in _LINES*
+split -l "${_CHUNKS}" "${_PATH}" _CHUNKS
+
+for _CHUNK in _CHUNKS*
 do
-	if (( ${_INDEX} == 1 )); then
+	if (( ${_INDEX} > 1 )); then
         echo ${_HEADER} > ${_DIR}/${_NAME}-chunk-${_INDEX}.csv
 	fi
-	cat ${_LINE} >> ${_DIR}/${_NAME}-chunk-${_INDEX}.csv
-	rm ${_LINE}
-	echo ${_INDEX}
+	cat ${_CHUNK} >> ${_DIR}/${_NAME}-chunk-${_INDEX}.csv
+	rm ${_CHUNK}
 	((_INDEX++))
 done
